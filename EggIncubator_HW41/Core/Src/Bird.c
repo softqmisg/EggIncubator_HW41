f27d79f0b6e2c2ebfd564a84be88812fa0778dde
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2022-04-16 16:07:19 +0430

    update Temp/Humid
    Set Temp/Humid/Timing for different Birds

--- EggIncubator_HW41/Core/Src/Bird.c
+++ EggIncubator_HW41/Core/Src/Bird.c
@@ -9,39 +9,43 @@ Prog_t Manual_prog[2]={
 
 };
 Prog_t Morgh_prog[2]={
-		{.humidty=600,.temperature=377,.durationDays=18},//setter
+		{.humidty=550,.temperature=377,.durationDays=18},//setter
 		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t ShotorMorgh_prog[2]={
-		{.humidty=220,.temperature=363,.durationDays=18},//setter
-		{.humidty=270,.temperature=366,.durationDays=3},//hatcher
+		{.humidty=250,.temperature=363,.durationDays=39},//setter
+		{.humidty=300,.temperature=366,.durationDays=3},//hatcher
 };
-Prog_t Ghaz_prog[2]={
-		{.humidty=750,.temperature=375,.durationDays=18},//setter
-		{.humidty=850,.temperature=375,.durationDays=3},//hatcher
+Prog_t Ghaz_prog[4]={
+		{.humidty=600,.temperature=380,.durationDays=7},//setter1
+		{.humidty=580,.temperature=378,.durationDays=7},//setter2
+		{.humidty=620,.temperature=375,.durationDays=7},//setter3
+		{.humidty=750,.temperature=372,.durationDays=7},//hatcher
 };
 Prog_t Ordak_prog[2]={
-		{.humidty=700,.temperature=375,.durationDays=18},//setter
-		{.humidty=800,.temperature=375,.durationDays=3},//hatcher
+		{.humidty=600,.temperature=377,.durationDays=25},//setter
+		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t Kabk_prog[2]={
-		{.humidty=570,.temperature=375,.durationDays=18},//setter
-		{.humidty=750,.temperature=370,.durationDays=3},//hatcher
+		{.humidty=550,.temperature=377,.durationDays=21},//setter
+		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t Gharghavol_prog[2]={
-		{.humidty=650,.temperature=366,.durationDays=18},//setter
-		{.humidty=800,.temperature=372,.durationDays=3},//hatcher
+		{.humidty=550,.temperature=377,.durationDays=22},//setter
+		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t Belderchin_prog[2]={
-		{.humidty=650,.temperature=377,.durationDays=18},//setter
-		{.humidty=770,.temperature=372,.durationDays=3},//hatcher
+		{.humidty=550,.temperature=377,.durationDays=14},//setter
+		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t Boghalamoon_prog[2]={
-		{.humidty=600,.temperature=375,.durationDays=18},//setter
-		{.humidty=700,.temperature=375,.durationDays=3},//hatcher
+		{.humidty=550,.temperature=380,.durationDays=7},//setter1
+		{.humidty=520,.temperature=377,.durationDays=7},//setter2
+		{.humidty=600,.temperature=375,.durationDays=7},//setter3
+		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 Prog_t MorghShakhdar_prog[2]={
-		{.humidty=600,.temperature=375,.durationDays=18},//setter
+		{.humidty=550,.temperature=377,.durationDays=23},//setter
 		{.humidty=700,.temperature=372,.durationDays=3},//hatcher
 };
 
@@ -49,12 +53,12 @@ Bird_t	defaultBirds[NumOfBirds]={
 	{.Type=Manual,				.TotalDurationDays=21,.HatchTotalDays=0,.NumofProg=2,.pProgs=Manual_prog,				},//Manual
 	{.Type=Morgh,					.TotalDurationDays=21,.HatchTotalDays=3,.NumofProg=2,.pProgs=Morgh_prog,				},//Morgh
 	{.Type=MorghShakhdar,	.TotalDurationDays=28,.HatchTotalDays=3,.NumofProg=2,.pProgs=MorghShakhdar_prog,},//MorghShakhdar
-	{.Type=Boghalamoon,		.TotalDurationDays=28,.HatchTotalDays=3,.NumofProg=2,.pProgs=Boghalamoon_prog,	},//Boghalamoon
+	{.Type=Boghalamoon,		.TotalDurationDays=24,.HatchTotalDays=3,.NumofProg=4,.pProgs=Boghalamoon_prog,	},//Boghalamoon
 	{.Type=Belderchin,		.TotalDurationDays=17,.HatchTotalDays=3,.NumofProg=2,.pProgs=Belderchin_prog,		},//Belderchin
 	{.Type=Gharghavol,		.TotalDurationDays=26,.HatchTotalDays=3,.NumofProg=2,.pProgs=Gharghavol_prog,		},//Gharghavol
 	{.Type=Kabk,					.TotalDurationDays=24,.HatchTotalDays=3,.NumofProg=2,.pProgs=Kabk_prog,					},//Kabk
 	{.Type=Ordak,					.TotalDurationDays=28,.HatchTotalDays=3,.NumofProg=2,.pProgs=Ordak_prog,				},//Ordak
-	{.Type=Ghaz,					.TotalDurationDays=35,.HatchTotalDays=3,.NumofProg=2,.pProgs=Ghaz_prog,					},//Ghaz
+	{.Type=Ghaz,					.TotalDurationDays=28,.HatchTotalDays=7,.NumofProg=4,.pProgs=Ghaz_prog,					},//Ghaz
 	{.Type=ShotorMorgh,		.TotalDurationDays=42,.HatchTotalDays=3,.NumofProg=2,.pProgs=ShotorMorgh_prog,	},//ShotorMorgh
 };
 
